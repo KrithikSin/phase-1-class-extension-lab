df756789d19c38a16a44eb88658b442fedb673d0
Krithik Singh <krithiksingh@gmail.com>
2023-11-29 10:32:32 -0500

    class extensions lab completed

--- index.js
+++ index.js
@@ -1,1 +1,53 @@
 // Your code here
+class Polygon {
+    constructor(sidesOfShape) {
+        this.sidesOfShape = sidesOfShape;
+    }
+
+    get countSides() {
+        const amountOfSides = this.sidesOfShape.length;
+        return amountOfSides;
+    }
+
+    get perimeter() {
+        const perimeter = this.sidesOfShape.reduce((accumulator, currValue) => {
+            return accumulator + currValue;
+        });
+        return perimeter;
+    }
+}
+
+class Triangle extends Polygon {
+    get isValid() {
+        if (
+            this.sidesOfShape[0] + this.sidesOfShape[1] >
+                this.sidesOfShape[2] &&
+            this.sidesOfShape[1] + this.sidesOfShape[2] >
+                this.sidesOfShape[0] &&
+            this.sidesOfShape[0] + this.sidesOfShape[2] > this.sidesOfShape[1]
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+class Square extends Polygon {
+    // [4, 4, 4, 4]
+    get isValid() {
+        if (
+            this.sidesOfShape[0] === this.sidesOfShape[1] &&
+            this.sidesOfShape[0] === this.sidesOfShape[2] &&
+            this.sidesOfShape[0] === this.sidesOfShape[3]
+        ) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    get area() {
+        return this.sidesOfShape[0] * this.sidesOfShape[1];
+    }
+}
